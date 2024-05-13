cc6d15eedb44711e43fd79f458eceb135cbbc7f6
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-05-14 00:23:17 +0530

    Update the font style

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
